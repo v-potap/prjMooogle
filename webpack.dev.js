778bf80fd3dc98fa12460452def33814a8fcdb97
v-potap <v.o.potapenko@gmail.com>
2019-08-14 12:43:05 +0300

    init

---
+++ webpack.dev.js
@@ -0,0 +1,79 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackBar = require('webpackbar')
+
+module.exports = {
+
+  // This option controls if and how source maps are generated.
+  // https://webpack.js.org/configuration/devtool/
+  devtool: 'eval-cheap-module-source-map',
+
+  // https://webpack.js.org/concepts/entry-points/#multi-page-application
+  entry: {
+    list: './src/page-list/main.js',
+    details: './src/page-details/main.js',
+  },
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    port: 8000,
+    writeToDisk: false // https://webpack.js.org/configuration/dev-server/#devserverwritetodisk-
+  },
+
+  // https://webpack.js.org/concepts/loaders/
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+          // Please note we are not running postcss here
+        ]
+      },
+      {
+        // Load all images as base64 encoding if they are smaller than 8192 bytes
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // On development we want to see where the file is coming from, hence we preserve the [path]
+              name: '[path][name].[ext]?hash=[hash:20]',
+              limit: 8192
+            }
+          }
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        use: 'handlebars-loader'
+      }
+    ]
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/page-list/index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/page-details/details.html',
+      inject: true,
+      chunks: ['details'],
+      filename: 'details.html'
+    }),
+    new WebpackBar()
+  ]
+}
