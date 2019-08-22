e6663383d939c6f9659ab7e04248d652367c4c0d
Dmitriy <velkov55@gmail.com>
2019-08-22 14:56:15 +0300

    added files header

--- src/js/header.js
+++ src/js/header.js
@@ -5,52 +5,64 @@ import movieDBService from "../js/api-service";
 class App {
   constructor() {
     this.refs = {};
-    this.refs.navButton = document.querySelector('.header__siteNav__button');
-    this.refs.siteNavMenu = document.querySelector('.header__overlay');
-    this.refs.menuCategory = document.querySelector('.header__siteNavMenu');
-    this.refs.itemCategoryMenu = document.querySelectorAll('.siteNavCat__item');
-    this.refs.searchInput = document.querySelector('.header__search');
+    this.refs.navButton = document.querySelector(".header__siteNav__button");
+    this.refs.siteNavMenu = document.querySelector(".header__overlay");
+    this.refs.menuCategory = document.querySelector(".header__siteNavMenu");
+    this.refs.itemCategoryMenu = document.querySelectorAll(".siteNavCat__item");
+    this.refs.searchInput = document.querySelector(".header__search");
 
-    this.refs.navButton.addEventListener('click', this.handleClick.bind(this));
-    this.refs.menuCategory.addEventListener('click', this.handleClickTitle.bind(this));
-    this.refs.searchInput.addEventListener('click', this.handleClickSearchInput.bind(this));
-
+    this.refs.navButton.addEventListener("click", this.handleClick.bind(this));
+    this.refs.menuCategory.addEventListener(
+      "click",
+      this.handleClickTitle.bind(this)
+    );
+    this.refs.searchInput.addEventListener(
+      "click",
+      this.handleClickSearchInput.bind(this)
+    );
   }
 
   handleClick() {
-    this.refs.siteNavMenu.classList.toggle('show');
-  };
+    this.refs.siteNavMenu.classList.toggle("show");
+  }
 
-async handleClickTitle(e) {
-    const li = e.target.closest('.header__siteNavCat');
-    const ul = li.querySelector('.siteNavCat__item');
-    this.refs.itemCategoryMenu.forEach(element => {
-      element.innerHTML = '';
-    });
-    if(!ul.hasChildNodes()) {
-      this.refs.menuCategory.classList.toggle('click');
-      const storage = e.target.closest('h2').textContent.toLowerCase();
-      if(storage === "favorites") {
-        const evt = new MouseEvent('click', {
-          bubbles: true,
-          cancelable: true,
-          view: window
+  async handleClickTitle(e) {
+    const li = e.target.closest(".header__siteNavCat");
+    const ul = li.querySelector(".siteNavCat__item");
+
+    this.refs.menuCategory.classList.toggle("click");
+    const storage = e.target.closest("h2").textContent.toLowerCase();
+
+    if (storage === "favorities" || ul.hasChildNodes()) {
+      const evt = new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window
       });
-      this.refs.siteNavMenu.classList.toggle('show');
-      document.querySelector("#radio-favorities").dispatchEvent(evt);
-      return;
-      }
-      movieDBService.setStorage(storage === "movie"?storage:"tv");
+
+      this.refs.itemCategoryMenu.forEach(element => {
+        element.innerHTML = "";
+      });
+
+      this.refs.siteNavMenu.classList.toggle("show");
+
+      document.querySelector(`#radio-${storage}`).dispatchEvent(evt);
+
+    } else {
+      let st = storage;
+      st = (st === "series" ? "tv" : st);
+      st = (st === "movies" ? "movie" : st);
+
+      movieDBService.setStorage(st);
       const listCategory = await movieDBService.getGenres();
       const markup = listCategory.map(el => itemCategory(el)).join("");
       ul.insertAdjacentHTML("beforeend", markup);
     }
-  };
+  }
 
   handleClickSearchInput(e) {
-    e.currentTarget.classList.toggle('click');
-  };
-
+    e.currentTarget.classList.toggle("click");
+  }
 }
 
 new App();
